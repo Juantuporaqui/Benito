8fa3da1caedbe4a02e800e77078052ff55333c2d
Juantuporaqui <juantuporaqui@yahoo.es>
2025-06-14 22:12:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth, signInAnonymously, onAuthStateChanged, signInWithCustomToken } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore, collection, doc, addDoc, setDoc, getDoc, getDocs, query, where, serverTimestamp, Timestamp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+// --- Firebase Configuration ---
+const firebaseConfig = {
+    apiKey: "AIzaSyDTvriR7KjlAINO44xhDDvIDlc4T_4nilo",
+    authDomain: "ucrif-5bb75.firebaseapp.com",
+    projectId: "ucrif-5bb75",
+    storageBucket: "ucrif-5bb75.firebasestorage.app",
+    messagingSenderId: "241698436443",
+    appId: "1:241698436443:web:1f333b3ae3f813b755167e",
+    measurementId: "G-S2VPQNWZ21"
+};
