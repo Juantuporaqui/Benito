ad696c0cfd36b7ea600d228c7d5ff16ff426d2ff
Juantuporaqui <juantuporaqui@yahoo.es>
2025-06-20 09:33:12 +0200

    Create resumen.js

---
+++ src/modules/views/resumen.js
@@ -0,0 +1,6 @@
+diff --git a//dev/null b/src/modules/views/resumen.js
+index 0000000000000000000000000000000000000000..43ed5e2ebcb7b591f1a7ce36e054026fa0790fcb 100644
+--- a//dev/null
++++ b/src/modules/views/resumen.js
+@@ -0,0 +1 @@
++export { renderResumen } from '../../ui/views.js';
