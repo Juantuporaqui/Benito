19e61bfc0c1911e3556da2abf02f97d7d4f0f112
Juantuporaqui <juantuporaqui@yahoo.es>
2025-06-20 09:34:13 +0200

    Create statistics.js

---
+++ src/modules/views/statistics.js
@@ -0,0 +1,6 @@
+diff --git a//dev/null b/src/modules/views/statistics.js
+index 0000000000000000000000000000000000000000..2ec10ef118c68f7e47d8b89a882d02681e4bb494 100644
+--- a//dev/null
++++ b/src/modules/views/statistics.js
+@@ -0,0 +1 @@
++export { renderStatistics } from '../../ui/views.js';
