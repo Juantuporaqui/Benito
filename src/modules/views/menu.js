0c837828c12f7effab02e4c7107f97f4f8ebd0f0
Juantuporaqui <juantuporaqui@yahoo.es>
2025-06-20 09:55:09 +0200

    Update menu.js

--- src/modules/views/menu.js
+++ src/modules/views/menu.js
@@ -1,6 +1,1 @@
-diff --git a//dev/null b/src/modules/views/menu.js
-index 0000000000000000000000000000000000000000..43efd1054002bb002f9629aa73a561eea871781c 100644
---- a//dev/null
-+++ b/src/modules/views/menu.js
-@@ -0,0 +1 @@
-+export { renderMenu } from '../../ui/views.js';
+export { renderMenu } from '../../ui/views.js';
