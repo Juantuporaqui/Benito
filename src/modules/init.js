ae006e265a8304cc74d34cc87160da05060f9e33
Juantuporaqui <juantuporaqui@yahoo.es>
2025-06-20 09:56:51 +0200

    Update init.js

--- src/modules/init.js
+++ src/modules/init.js
@@ -1,6 +1,1 @@
-diff --git a//dev/null b/src/modules/init.js
-index 0000000000000000000000000000000000000000..c71267f3f8386661a0493555b86522831410603a 100644
---- a//dev/null
-+++ b/src/modules/init.js
-@@ -0,0 +1 @@
-+export { initViews as init } from '../ui/views.js';
+export { initViews as init } from '../ui/views.js';
